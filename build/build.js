8b7af6d9a9464f2b1cd8241b3124fedf17eceace
Max Peshkov <peshkov1.max@gmail.com>
2017-08-23 14:20:29 +0600

    Fix paths

--- build/build.js
+++ build/build.js
@@ -9,8 +9,11 @@ const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
-
+const fs = require('fs')
+const ncp = require('ncp').ncp
 const spinner = ora('building for production...')
+const mkdirp = require('mkdirp')
+
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
@@ -31,5 +34,44 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
+
+    // copy src/robots.txt, sitemap.xml to needed locations
+    fs.createReadStream(path.resolve(__dirname, '../src/robots.txt'))
+      .pipe(fs.createWriteStream(path.resolve(__dirname, '../robots.txt')))
+
+    fs.createReadStream(path.resolve(__dirname, '../src/sitemap.xml'))
+      .pipe(fs.createWriteStream(path.resolve(__dirname, '../public/sitemap.xml')))
+
+    mkdirp(path.resolve(__dirname, '../public/prerendered'))
+    mkdirp(path.resolve(__dirname, '../public/prerendered/ru'))
+    mkdirp(path.resolve(__dirname, '../public/prerendered/ua'))
+    mkdirp(path.resolve(__dirname, '../public/prerendered/en'))
+
+    // copy prerendered from project root to public/prerendered
+    ncp(path.resolve(__dirname, '../en'), path.resolve(__dirname, '../public/prerendered/en'), function (err) {
+      if (err) {return console.error(err)}
+      console.log('en copied!')
+      rm(path.resolve(__dirname, '../en'), err => {
+        if (err) {return console.error(err)}
+        console.log('en removed!')
+      })
+    })
+    ncp(path.resolve(__dirname, '../ua'), path.resolve(__dirname, '../public/prerendered/ua'), function (err) {
+      if (err) {return console.error(err)}
+      console.log('ua copied!')
+      rm(path.resolve(__dirname, '../ua'), err => {
+        if (err) {return console.error(err)}
+        console.log('ua removed!')
+      })
+    })
+    ncp(path.resolve(__dirname, '../ru'), path.resolve(__dirname, '../public/prerendered/ru'), function (err) {
+      if (err) {return console.error(err)}
+      console.log('ru copied!')
+      // remove prerendered from project root
+      rm(path.resolve(__dirname, '../ru'), err => {
+        if (err) {return console.error(err)}
+        console.log('ru removed!')
+      })
+    })
   })
 })
